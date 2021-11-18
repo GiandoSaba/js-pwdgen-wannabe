f5b9b235c5efcb5b54198c15e8697ec48e3f1fa3
GiandoSaba <giandosaba997@gmail.com>
2021-11-18 15:18:14 +0100

    end

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+let nome = prompt('Inserisci il tuo nome');
+let cognome = prompt('Inserisci il tuo cognome');
+let colore = prompt('Inserisci il tuo colore preferito');
+const numero = 21;
+let password = nome + cognome + colore + numero;
+console.log(password);
+
+document.getElementById("my_div").innerHTML =
+`
+    <h1> La tua password generata è: ${password} </h1>
+
+`;
